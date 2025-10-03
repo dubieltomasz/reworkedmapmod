8f72a2c03451e3b3eccc2238af802c1fd9e13541
Tomasz Dubiel <dubieltomasz420@gmail.com>
2025-10-03 22:41:05 +0200

    Added new item 'Reworked Map'

--- src/main/java/io/tobert/reworkedmapmod/ReworkedMapMod.java
+++ src/main/java/io/tobert/reworkedmapmod/ReworkedMapMod.java
@@ -1,5 +1,6 @@
 package io.tobert.reworkedmapmod;
 
+import io.tobert.reworkedmapmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -19,6 +20,6 @@ public class ReworkedMapMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/io/tobert/reworkedmapmod/item/ModItems.java
+++ src/main/java/io/tobert/reworkedmapmod/item/ModItems.java
@@ -6,17 +6,19 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item REWORKED_MAP = registerItem("reworked_map", new Item(new Item.Settings()));
+    public static final Item REWORKED_MAP = registerItem("reworked_map", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ReworkedMapMod.MOD_ID,"reworked_map")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(ReworkedMapMod.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
-        ReworkedMapMod.LOGGER.info("Registering Mod Items for " + ReworkedMapMod.MOD_ID);
+        ReworkedMapMod.LOGGER.info("Register mod items for " + ReworkedMapMod.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(REWORKED_MAP);
