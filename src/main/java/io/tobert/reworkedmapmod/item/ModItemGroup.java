c3c4db6b97a68868e70e0614dc07c7f5e28e7124
Tomasz Dubiel <dubieltomasz420@gmail.com>
2025-10-05 14:43:11 +0200

    Changed some logic and cleaned code

--- src/main/java/io/tobert/reworkedmapmod/item/ModItemGroup.java
+++ src/main/java/io/tobert/reworkedmapmod/item/ModItemGroup.java
@@ -6,19 +6,20 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
-    public static final ItemGroup REWORKED_MAP_MOD_GROUP = Registry.register(Registries.ITEM_GROUP,
-            Identifier.of(ReworkedMapMod.MOD_ID, "reworked_map_mod"),
-            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.REWORKED_MAP))
-                    .displayName(Text.translatable("itemgroup.reworkedmapmod.reworked_map_mod"))
-                    .entries((displayContext, entries) -> {
-                        entries.add(ModItems.REWORKED_MAP);
-                    }).build());
+    public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(),
+            Identifier.of(ReworkedMapMod.MOD_ID, "reworked_map_mod"));
+    public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
+            .displayName(Text.translatable("itemgroup.reworkedmapmod.reworked_map_mod_group"))
+            .icon(() -> new ItemStack(ModItems.REWORKED_MAP))
+            .build();
 
     public static void registerItemGroup() {
         ReworkedMapMod.LOGGER.info("Registering Item Group for " + ReworkedMapMod.MOD_ID);
+        Registry.register(Registries.ITEM_GROUP, CUSTOM_ITEM_GROUP_KEY, CUSTOM_ITEM_GROUP);
     }
 }
--- src/main/java/io/tobert/reworkedmapmod/item/ModItems.java
+++ src/main/java/io/tobert/reworkedmapmod/item/ModItems.java
@@ -9,21 +9,25 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import java.util.function.Function;
 
 public class ModItems {
-    public static final Item REWORKED_MAP = registerItem("reworked_map", new Item(
-            new Item.Settings().registryKey(RegistryKey.of(
-                    RegistryKeys.ITEM, Identifier.of(ReworkedMapMod.MOD_ID,"reworked_map")
-            ))
-    ));
+    public static final Item REWORKED_MAP = registerHelper("reworked_map", Item::new, new Item.Settings());
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(ReworkedMapMod.MOD_ID, name), item);
+    public static Item registerHelper(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ReworkedMapMod.MOD_ID, name));
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
     }
 
     public static void registerModItems() {
         ReworkedMapMod.LOGGER.info("Register mod items for " + ReworkedMapMod.MOD_ID);
 
+        ItemGroupEvents.modifyEntriesEvent(ModItemGroup.CUSTOM_ITEM_GROUP_KEY).register(entries -> {
+            entries.add(REWORKED_MAP);
+        });
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(REWORKED_MAP);
         });
