2631da686df60eaf21876184339c206898ab62af
Tomasz Dubiel <dubieltomasz420@gmail.com>
2025-10-22 20:42:59 +0200

    Mixin preparation

--- src/main/java/io/tobert/reworkedmapmod/mixin/ExampleMixin.java
+++ src/main/java/io/tobert/reworkedmapmod/mixin/ExampleMixin.java
@@ -1,15 +1,20 @@
 package io.tobert.reworkedmapmod.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import io.tobert.reworkedmapmod.ReworkedMapMod;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(MinecraftClient.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+	@Inject(method = "setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", at = @At("TAIL"))
+	private void aSetScreen(Screen screen, CallbackInfo ci) {
+		if (screen instanceof InventoryScreen) {
+			ReworkedMapMod.LOGGER.info("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		}
 	}
 }
